e6ec5c508491e41799174e0af79f8f94dd22e471
Jim Girardeau <jgirardeau@uptime-solutions.us>
2018-11-08 10:58:53 -0600

    cleaned up formatting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
+// $(window).load(function () {
 var topic = "star trek";
 var items = ["kirk", "spock", "scotty", "uhuru", "checkov", "khan", "mccoy", "sulu", "data"];
 //  "worf", "riker", "troi", "crusher", "geordi", "sulu"];
 function build_game() {
-    items.forEach(function(value) {
+    items.forEach(function (value) {
         var new_button = $("<button>");
         new_button.text(value);
         new_button.addClass("trek btn btn-primary")
@@ -11,14 +12,14 @@ function build_game() {
 }
 
 
-$(".addTrek").click(function() {
+$(".addTrek").click(function () {
     event.preventDefault();
     var newItem = $("#input_text").val();
     var new_button = $("<button>");
     new_button.text(newItem);
     new_button.addClass("trek btn btn-primary");
     var indx = parseInt(items.indexOf(newItem));
-    if (indx == -1) {
+    if (indx == -1 &&newItem.length>0) {
         $("#buttons").append(new_button);
         items.push(newItem);
     }
@@ -44,23 +45,23 @@ function add_giphy_to_html(results) {
 }
 
 // $(".trek").on("click", function() {
-$(document).on('click', '.trek', function() {
+$(document).on('click', '.trek', function () {
     event.preventDefault();
     var person = $(this).text();
     var queryURL = 'https://api.giphy.com/v1/gifs/search?q=star trek ' + person + '&api_key=dc6zaTOxFJmzC&limit=10';
     //   console.log(queryURL);
     $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-        .then(function(response) {
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
             //  console.log(response);
             var results = response.data;
             add_giphy_to_html(response.data);
         });
 });
 
-$(document).on('click', '.gifClick', function() {
+$(document).on('click', '.gifClick', function () {
     event.preventDefault();
     // console.log(this);
     var moving = parseInt($(this).attr('moving'));
@@ -76,3 +77,4 @@ $(document).on('click', '.gifClick', function() {
 });
 
 build_game();
+// });
